9e2fe773b3682a5d7c752d1084467ddcb04ea56c
Bilal Durrani <bilal33@gmail.com>
2020-01-07 23:02:38 -0500

    starting to support multiple files

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{App, Arg};
 use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader, StdoutLock, Write, ErrorKind};
+use std::io::{self, BufRead, BufReader, ErrorKind, StdoutLock, Write};
 use std::path::PathBuf;
 use std::process::exit;
 
@@ -22,21 +22,34 @@ Usage:
     akv_mem FILE insert KEY VALUE
     akv_mem FILE update KEY VALUE
 ";
+/// Holds the values of the program input
+struct Program {
+    number: bool,
+    paths: Vec<String>,
+}
 
 fn main() {
     let matches = App::new("cat")
         .version("0.0.1")
         .about("Basic cat")
         .arg(Arg::with_name("show-all").short("f").long("show-all"))
+        .arg(Arg::with_name("number").short("n").long("number"))
         .arg(Arg::with_name("FILE").index(1).multiple(true))
         .get_matches();
 
-    //    let file = matches.value_of("FILE").unwrap_or_else(|| "");
+    //    let mut input_args = Program { number: false };
 
     let file = match matches.value_of("FILE") {
         Some(val) => val,
         None => "",
     };
+
+    let multiple_files = match matches.values_of("FILE") {
+        Some(val) => val.collect(),
+        None => vec![""; 0],
+    };
+
+    println!("multiple files {:?}", multiple_files);
 
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
@@ -45,28 +58,33 @@ fn main() {
         print_or_die(&mut stdout, b"No such file or directory");
     }
 
-    if let Err(err) = cat(file, &mut stdout){
-        print_or_die(&mut stdout, b"Something happened");
+    if let Err(err) = cat(file, &mut stdout) {
+        if let Some(inner_error) = err.get_ref() {
+            eprintln!("{}", inner_error);
+        } else {
+            eprintln!("{}", err);
+        }
     }
 }
 
 fn cat(path: &str, stdout: &mut StdoutLock) -> io::Result<()> {
     let file = PathBuf::from(path);
     let metadata = fs::metadata(&file)?;
-    if metadata.is_dir(){
-        return Err(io::Error::new(ErrorKind::Other,"This is a directory"));
+    if metadata.is_dir() {
+        let err_message = format!("cat: {}: Is a directory", file.display());
+        return Err(io::Error::new(ErrorKind::Other, err_message));
     }
     let file = File::open(file)?;
     let mut reader = BufReader::new(file);
 
+    let mut line = String::new();
+
     loop {
-        let mut line = String::new();
         let num_read = reader.read_line(&mut line)?;
         if num_read == 0 {
             break;
         }
         stdout.write(line.as_bytes())?;
-        //        let num_read = file.read(&mut buf)?;
     }
     stdout.flush()?;
     Ok(())

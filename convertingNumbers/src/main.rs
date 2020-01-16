a50257780f184b2fd0c65fc05df002319fa75a5a
Bilal Durrani <bilal33@gmail.com>
2020-01-16 04:03:33 +0000

    print print

--- convertingNumbers/src/main.rs
+++ convertingNumbers/src/main.rs
@@ -1,21 +1,23 @@
 use std::mem;
 
 fn main() {
-    println!("size of u32: {}", mem::size_of::<u32>());
+    //    println!("size of u32: {}", mem::size_of::<u32>());
     let size = mem::size_of::<u32>();
+    println!("{0: <12} | {1: <12}", "size (bytes)", "verb");
     for item in vec![
         "GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH",
     ] {
         let mut trunc = String::from(item);
+        println!("{0: <12} | {1: <12}", size, trunc);
         // pad strings with spaces
         trunc = format!("{1: <0$}", size, trunc);
-        println!("{}", trunc);
-        trunc.truncate(4);
+        //        println!("{}", trunc);
+        trunc.truncate(size);
         let mut array: [u8; 4] = [0x20; 4];
         let raw_bytes = trunc.as_bytes();
         array.copy_from_slice(&raw_bytes);
         // array.copy_from_slice(raw_bytes);
-        println!("{:x?}", array);
+        //        println!("{:x?}", array);
     }
     // returns raw bytes as a slice
     // https://doc.rust-lang.org/std/primitive.str.html#method.as_bytes

6f9abd6668f0b75012b258da01cf6b92da9940a7
Bilal Durrani <bilal33@gmail.com>
2020-01-17 03:16:15 +0000

    added a shitty implementation

--- convertingNumbers/src/main.rs
+++ convertingNumbers/src/main.rs
@@ -1,27 +1,43 @@
-use std::mem;
+use std::mem::size_of;
 
 fn main() {
-    //    println!("size of u32: {}", mem::size_of::<u32>());
-    let size = mem::size_of::<u32>();
-    println!("{0: <12} | {1: <12}", "size (bytes)", "verb");
-    for item in vec![
-        "GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH",
-    ] {
-        let mut trunc = String::from(item);
-        println!("{0: <12} | {1: <12}", size, trunc);
-        // pad strings with spaces
-        trunc = format!("{1: <0$}", size, trunc);
-        //        println!("{}", trunc);
-        trunc.truncate(size);
-        let mut array: [u8; 4] = [0x20; 4];
-        let raw_bytes = trunc.as_bytes();
-        array.copy_from_slice(&raw_bytes);
-        // array.copy_from_slice(raw_bytes);
-        //        println!("{:x?}", array);
+    println!(
+        "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
+        "size (bytes)", "verb", "big-endian", "little-endian"
+    );
+    for size in vec![size_of::<u16>(), size_of::<u32>()] {
+        for item in vec![
+            "GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "TRACE", "PATCH",
+        ] {
+            let mut trunc = String::from(item);
+            // pad strings with spaces
+            trunc = format!("{1: <0$}", 4, trunc);
+            trunc.truncate(size);
+            if size == 4 {
+                let mut array: [u8; 4] = [0x20; 4];
+                let raw_bytes = trunc.as_bytes();
+                //            println!("before slice {:?}", raw_bytes);
+                //            array.copy_from_slice(&[raw_bytes[0], raw_bytes[1], raw_bytes[2], raw_bytes[3]]);
+                array.copy_from_slice(&raw_bytes[..4]);
+                println!(
+                    "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
+                    size,
+                    item,
+                    u32::from_be_bytes(array),
+                    u32::from_le_bytes(array)
+                );
+            } else {
+                let mut array: [u8; 2] = [0x20; 2];
+                let raw_bytes = trunc.as_bytes();
+                array.copy_from_slice(&raw_bytes[..2]);
+                println!(
+                    "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
+                    size,
+                    item,
+                    u16::from_be_bytes(array),
+                    u16::from_le_bytes(array)
+                );
+            }
+        }
     }
-    // returns raw bytes as a slice
-    // https://doc.rust-lang.org/std/primitive.str.html#method.as_bytes
-    // let raw_bytes_slice = command.as_bytes();
-    // let raw_bytes_vector = raw_bytes_slice.to_ve
-    // println!("raw_bytes {:?}", raw_bytes);
 }

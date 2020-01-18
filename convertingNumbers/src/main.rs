8b81cc4401aa5eba59341dc78ce50d005f3513d3
Bilal Durrani <bilal33@gmail.com>
2020-01-18 18:30:06 -0500

    lets stick with this for now

--- convertingNumbers/src/main.rs
+++ convertingNumbers/src/main.rs
@@ -1,9 +1,11 @@
 use std::mem::size_of;
 
 fn main() {
+    let column_width = 10;
+
     println!(
-        "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
-        "size (bytes)", "verb", "big-endian", "little-endian"
+        "{0: <4$} | {1: <4$} | {2: <4$} | {3: <4$}",
+        "size", "verb", "big-endian", "little-endian", column_width
     );
     for size in vec![size_of::<u16>(), size_of::<u32>()] {
         for item in vec![
@@ -13,31 +15,33 @@ fn main() {
             // pad strings with spaces
             trunc = format!("{1: <0$}", 4, trunc);
             trunc.truncate(size);
+            let be_string: String;
+            let le_string: String;
+
             if size == 4 {
                 let mut array: [u8; 4] = [0x20; 4];
                 let raw_bytes = trunc.as_bytes();
-                //            println!("before slice {:?}", raw_bytes);
-                //            array.copy_from_slice(&[raw_bytes[0], raw_bytes[1], raw_bytes[2], raw_bytes[3]]);
                 array.copy_from_slice(&raw_bytes[..4]);
-                println!(
-                    "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
-                    size,
-                    item,
-                    u32::from_be_bytes(array),
-                    u32::from_le_bytes(array)
-                );
-            } else {
+                be_string = u32::from_be_bytes(array).to_string();
+                le_string = u32::from_le_bytes(array).to_string();
+            } else if size == 2 {
                 let mut array: [u8; 2] = [0x20; 2];
                 let raw_bytes = trunc.as_bytes();
                 array.copy_from_slice(&raw_bytes[..2]);
-                println!(
-                    "{0: <12} | {1: <12} | {2: <12} | {3: <12}",
-                    size,
-                    item,
-                    u16::from_be_bytes(array),
-                    u16::from_le_bytes(array)
-                );
+                be_string = u16::from_be_bytes(array).to_string();
+                le_string = u16::from_le_bytes(array).to_string();
+            } else {
+                unimplemented!();
             }
+
+            println!(
+                "{0: <4$} | {1: <4$} | {2: <4$} | {3: <4$}",
+                size,
+                item,
+                be_string,
+                le_string,
+                column_width
+            );
         }
     }
 }

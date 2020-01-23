7b525020f8ed65cfa6ce4b2e4800b60bd268c452
Bilal Durrani <bilal33@gmail.com>
2020-01-22 22:48:48 -0500

    working thru the conversion

--- parrotify/src/constants.rs
+++ parrotify/src/constants.rs
@@ -6,3 +6,338 @@ pub const ENCODED_A: [& 'static [u8;9];5] = [
     b"ABAAAAABA",
     b"ABAAAAABA"
 ];
+pub const encodedB : [& 'static [u8;9];5]= [
+    b"ABBBBBBAA",
+    b"ABAAAAABA",
+    b"ABBBBBBBA",
+    b"ABAAAAABA",
+    b"ABBBBBBAA",
+];
+​
+const encodedC : [& 'static [u8;9];5]= [
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABAAAAAAA",
+    b"ABAAAAAAA",
+    b"ABBBBBBBA",
+];
+​
+const encodedD : [& 'static [u8;9];5]= [
+    b"ABBBBBBAA",
+    b"ABAAAAABA",
+    b"ABAAAAABA",
+    b"ABAAAAABA",
+    b"ABBBBBBAA",
+];
+
+const encodedE: [& 'static [u8;9];5] = [
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABBBBBBBA",
+];
+​
+const encodedF: [& 'static [u8;9];5] = [
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABAAAAAAA",
+];
+​
+const encodedG : [& 'static [u8;9];5]= [
+    b"ABBBBBBBA",
+    b"ABAAAAAAA",
+    b"ABAAAABBA",
+    b"ABAAAAABA",
+    b"ABBBBBBBA",
+];
+​
+const encodedH = [
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+];
+​
+const encodedI = [
+    "ABBBBBBBA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "ABBBBBBBA",
+];
+​
+const encodedJ = [
+    "ABBBBBBBA",
+    "AAAAAABAA",
+    "AAAAAABAA",
+    "ABAAAABAA",
+    "ABBBBBBAA",
+];
+​
+const encodedK = [
+    "ABAAAAABA",
+    "ABAAABBAA",
+    "ABBBBBAAA",
+    "ABAAABBAA",
+    "ABAAAAABA",
+];
+​
+const encodedL = [
+    "ABAAAAAAA",
+    "ABAAAAAAA",
+    "ABAAAAAAA",
+    "ABAAAAAAA",
+    "ABBBBBBBA",
+];
+​
+const encodedM = [
+    "ABBAAABBA",
+    "ABABABABA",
+    "ABAABAABA",
+    "ABAABAABA",
+    "ABAABAABA",
+];
+​
+const encodedN = [
+    "ABBAAAABA",
+    "ABABAAABA",
+    "ABAABBABA",
+    "ABAAAABBA",
+    "ABAAAAABA",
+];
+​
+const encodedO = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encodedP = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+    "ABAAAAAAA",
+    "ABAAAAAAA",
+];
+​
+const encodedQ = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABAAABABA",
+    "ABBBBABAA",
+];
+​
+const encodedR = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+    "ABAAAABAA",
+    "ABAAAAABA",
+];
+​
+const encodedS = [
+    "ABBBBBBBA",
+    "ABAAAAAAA",
+    "AABBBBBAA",
+    "AAAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encodedT = [
+    "ABBBBBBBA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+];
+​
+const encodedU = [
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encodedV = [
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "ABAAAAABA",
+    "AABAAABAA",
+    "AAABBBAAA",
+];
+​
+const encodedW = [
+    "ABAAAAABA",
+    "ABAABAABA",
+    "ABAABAABA",
+    "ABABBBABA",
+    "ABBAAABBA",
+];
+​
+const encodedX = [
+    "ABAAAAABA",
+    "AABAAABAA",
+    "AAABBBAAA",
+    "AABAAABAA",
+    "ABAAAAABA",
+];
+​
+const encodedY = [
+    "ABAAAAABA",
+    "AABAAABAA",
+    "AAABBBAAA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+];
+​
+const encodedZ = [
+    "ABBBBBBBA",
+    "AAAAAABAA",
+    "AAAABBAAA",
+    "AABAAAAAA",
+    "ABBBBBBBA",
+];
+​
+const encodedSpace = [
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+];
+​
+const encodedExclamation = [
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "AAAAAAAAA",
+    "AAAABAAAA",
+];
+​
+const encodedQuestion = [
+    "ABBBBBBBA",
+    "AAAAAAABA",
+    "AAAABBBA",
+    "AAAAAAAAA",
+    "AAAABAAAA",
+];
+​
+const encodedPeriod = [
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "AAAABAAAA",
+];
+​
+const encodedDash = [
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+    "ABBBBBBBA",
+    "AAAAAAAAA",
+    "AAAAAAAAA",
+];
+​
+const encodedEquals = [
+    "AAAAAAAAA",
+    "ABBBBBBBA",
+    "AAAAAAAAA",
+    "ABBBBBBBA",
+    "AAAAAAAAA",
+];
+​
+const encodedPlus = [
+    "AAAABAAAA",
+    "AAAABAAAA",
+    "ABBBBBBBA",
+    "AAAABAAAA",
+    "AAAABAAAA",
+];
+​
+const encoded1 = [
+    "AAAABBAAA",
+    "AAABABAAA",
+    "AAAAABAAA",
+    "AAAAABAAA",
+    "AABBBBBBA",
+];
+​
+const encoded2 = [
+    "ABBBBBBBA",
+    "AAAAAAABA",
+    "ABBBBBBBA",
+    "ABAAAAAAA",
+    "ABBBBBBBA",
+];
+​
+const encoded3 = [
+    "ABBBBBBBA",
+    "AAAAAAABA",
+    "AABBBBBAA",
+    "AAAAAAABA",
+    "ABBBBBBAA",
+];
+​
+const encoded4 = [
+    "ABAAAABAA",
+    "ABAAAABAA",
+    "ABBBBBBBA",
+    "AAAAAABAA",
+    "AAAAAABAA",
+];
+​
+const encoded5 = [
+    "ABBBBBBBA",
+    "ABAAAAAAA",
+    "ABBBBBBBA",
+    "AAAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encoded6 = [
+    "ABBBBBBBA",
+    "ABAAAAAAA",
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encoded7 = [
+    "ABBBBBBBA",
+    "AAAAAABAA",
+    "AAAAABAAA",
+    "AAAABAAAA",
+    "AAABAAAAA",
+];
+​
+const encoded8 = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encoded9 = [
+    "ABBBBBBBA",
+    "ABAAAAABA",
+    "ABBBBBBBA",
+    "AAAAAAABA",
+    "ABBBBBBBA",
+];
+​
+const encoded0 = [
+    "ABBBBBBBA",
+    "ABAAAABBA",
+    "ABAABAABA",
+    "ABBAAAABA",
+    "ABBBBBBBA",
+];

b38cb6e08113d223acfc77892c348b31a9b6d080
Bilal Durrani <bilal33@gmail.com>
2020-01-22 22:12:54 -0500

    starting out

---
+++ parrotify/src/constants.rs
@@ -0,0 +1,8 @@
+
+pub const ENCODED_A: [& 'static [u8;9];5] = [
+    b"AABBBBBAA",
+    b"ABAAAAABA",
+    b"ABBBBBBBA",
+    b"ABAAAAABA",
+    b"ABAAAAABA"
+];
--- parrotify/src/main.rs
+++ parrotify/src/main.rs
@@ -1,3 +1,6 @@
+mod constants;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! {:?}", constants::ENCODED_A);
 }
+

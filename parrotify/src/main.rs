7810d78dca20ce69225284658cd0d51d468d2999
Bilal Durrani <bilal@hubdoc.com>
2020-01-23 17:38:56 -0500

    take input from command line

--- parrotify/src/main.rs
+++ parrotify/src/main.rs
@@ -1,6 +1,14 @@
+use clap::{App, Arg};
 mod constants;
 
 fn main() {
+    let matches = App::new("parrotify")
+        .version("0.0.1")
+        .about("parrot")
+        .arg(Arg::with_name("string1").short("s1").long("string1"))
+        .arg(Arg::with_name("string2").short("s2").long("string2"))
+        .arg(Arg::with_name("message").index(1))
+        .get_matches();
+
     println!("Hello, world! {:?}", constants::ENCODED_A);
 }
-

baf7de303b61240e587eac2578cb5343ca75fc58
Bilal Durrani <bilal33@gmail.com>
2020-01-27 15:57:40 -0500

    final result

--- module-test/src/foo.rs
+++
@@ -1,7 +0,0 @@
-mod bar;
-
-pub fn test_foo() {
-    println!("test_foo");
-    bar::test_bar();
-}
-
---
+++ module-test/src/foo/mod.rs
@@ -0,0 +1,6 @@
+mod bar;
+
+pub fn test_foo() {
+    println!("test_foo");
+    bar::test_bar();
+}

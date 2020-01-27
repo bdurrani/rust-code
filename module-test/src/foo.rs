6921ae89f23b3a3ec12b21767b1e2a34313f1fe0
Bilal Durrani <bilal33@gmail.com>
2020-01-27 13:08:02 -0500

    foo in its own file

---
+++ module-test/src/foo.rs
@@ -0,0 +1,18 @@
+use crate::constants;
+
+pub fn test_foo() {
+    println!("test_foo");
+    bar::test_bar();
+}
+
+mod bar {
+
+    pub fn test_bar() {
+        println!("bar {}", super::constants::CONSTANT_A);
+        test_bar_internal();
+    }
+
+    fn test_bar_internal() {
+        println!("test_bar_internal");
+    }
+}
--- module-test/src/main.rs
+++ module-test/src/main.rs
@@ -1,22 +1,5 @@
 mod constants;
-mod foo {
-    pub fn test_foo() {
-        println!("test_foo");
-        bar::test_bar();
-    }
-
-    mod bar {
-        use super::super::constants;
-        pub fn test_bar() {
-            println!("bar {}", constants::CONSTANT_A);
-            test_bar_internal();
-        }
-
-        fn test_bar_internal() {
-            println!("test_bar_internal");
-        }
-    }
-}
+mod foo;
 
 mod one {
     pub fn test1() {}

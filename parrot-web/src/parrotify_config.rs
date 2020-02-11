e7edc4db488870dccf85d6f065bbdfe9da49dc68
Bilal Durrani <bilal@hubdoc.com>
2020-02-11 17:38:15 -0500

    save work

--- parrot-web/src/parrotify_config.rs
+++ parrot-web/src/parrotify_config.rs
@@ -9,7 +9,8 @@ use actix_web::{web, HttpRequest, HttpResponse};
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/parrotify")
-            .route("/{text}", web::get().to(convert_text))
+//            .route("/{text}", web::get().to(convert_text))
+            .route("/{text}", web::get().to(convert_text_auto))
             .route("/{text}/{repl1}", web::get().to(convert_text_param1)),
     );
 }
@@ -24,6 +25,21 @@ async fn convert_text(req: HttpRequest) -> HttpResponse {
         println!("v2 {:?}", v2);
     }
     HttpResponse::Ok().body("Show users")
+}
+
+
+async fn convert_text_auto(info: web::Path<(String)>) -> HttpResponse {
+    let v1 = info.as_str();
+//    let v1 = req.match_info().get("text").unwrap();
+//    let v2 = req.match_info().query("repl1");
+    println!("v1 {:?}", v1);
+//    if v2.is_empty() {
+//        println!("v2 is empty");
+//    } else {
+//        println!("v2 {:?}", v2);
+//    }
+//    HttpResponse::Ok().body("Show users")
+    HttpResponse::Ok().body("hi")
 }
 
 async fn convert_text_param1(req: HttpRequest) -> HttpResponse {

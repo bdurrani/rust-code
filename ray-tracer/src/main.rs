0ec930773dcc8e91a9c07b139ff3d34516cdc542
Bilal Durrani <bilal33@gmail.com>
2020-02-16 19:05:24 -0500

    get sphere hit testing to work

--- ray-tracer/src/main.rs
+++ ray-tracer/src/main.rs
@@ -7,9 +7,27 @@ mod vec3;
 use ray::Ray;
 
 fn color(r: &Ray) -> Vec3 {
+    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let N: Vec3 = Vec3::unit_vector(r.point_at_parameter(t) - Vec3::new(0.0, 0.0, 1.0));
+        return 0.5 * Vec3::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
+    }
     let unit_direction: Vec3 = Vec3::unit_vector(r.direction());
     let t: f32 = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32 {
+    let oc = r.origin() - *center;
+    let a = Vec3::dot(&r.direction(), &r.direction());
+    let b = 2.0 * Vec3::dot(&oc, &r.direction());
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - f32::sqrt(discriminant)) / (2.0 * a)
+    }
 }
 
 fn main() {
--- ray-tracer/src/vec3.rs
+++ ray-tracer/src/vec3.rs
@@ -53,6 +53,18 @@ impl Vec3 {
     pub fn unit_vector(vector: Vec3) -> Vec3 {
         vector / vector.length()
     }
+
+    pub fn dot(v1: &Vec3, v2: &Vec3) -> f32 {
+        v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+    }
+
+    pub fn cross(v1: &Vec3, v2: &Vec3) -> Vec3 {
+        Vec3::new(
+            v1[1] * v2[2] - v1[2] * v2[1],
+            v1[2] * v2[0] - v1[0] * v2[2],
+            v1[0] * v2[1] - v1[1] * v2[0],
+        )
+    }
 }
 
 impl fmt::Display for Vec3 {

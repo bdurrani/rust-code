2e022c12fd57bb776c22fc472c3a2629370ca71a
Bilal Durrani <bilal33@gmail.com>
2020-02-21 17:22:08 -0500

    sigh. broke it

--- ray-tracer/src/main.rs
+++ ray-tracer/src/main.rs
@@ -1,5 +1,7 @@
 use crate::vec3::Vec3;
 use hittable::{HitRecord, Hittable};
+use hittableList::HitTableList;
+use sphere::Sphere;
 use std::f32;
 
 mod hittable;
@@ -12,32 +14,17 @@ use ray::Ray;
 
 fn color(r: &Ray, world: &Hittable) -> Vec3 {
     let mut rec = HitRecord::new();
-    if world.hit(&r, 0.0, f32::MAX, &mut rec) {
-        // TODO: Finish this
-    }
-    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r);
-    if t > 0.0 {
-        let N: Vec3 = Vec3::unit_vector(
-            r.point_at_parameter(t) - Vec3::new(0.0, 0.0, 1.0),
-        );
-        return 0.5 * Vec3::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
-    }
-    let unit_direction: Vec3 = Vec3::unit_vector(r.direction());
-    let t: f32 = 0.5 * (unit_direction.y() + 1.0);
-    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
-}
-
-fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32 {
-    let oc = r.origin() - *center;
-    let a = Vec3::dot(&r.direction(), &r.direction());
-    let b = 2.0 * Vec3::dot(&oc, &r.direction());
-    let c = Vec3::dot(&oc, &oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
-        -1.0
+    return if world.hit(&r, 0.0, f32::MAX, &mut rec) {
+        0.5 * Vec3::new(
+            rec.normal.x() + 1.0,
+            rec.normal.y() + 1.0,
+            rec.normal.z() + 1.0,
+        )
     } else {
-        (-b - f32::sqrt(discriminant)) / (2.0 * a)
-    }
+        let unit_direction: Vec3 = Vec3::unit_vector(r.direction());
+        let t: f32 = 0.5 * (unit_direction.y() + 1.0);
+        (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+    };
 }
 
 fn main() {
@@ -48,6 +35,11 @@ fn main() {
     let horizontal: Vec3 = Vec3::new(4.0, 0.0, 0.0);
     let vertical: Vec3 = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+    let mut list = HitTableList::new();
+    let sphere = Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5);
+    list.add(Box::new(sphere));
+    let sphere = Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0);
+    list.add(Box::new(sphere));
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u: f32 = i as f32 / nx as f32;
@@ -57,7 +49,8 @@ fn main() {
                 origin,
                 lower_left_corner + u * horizontal + v * vertical,
             );
-            let col: Vec3 = color(&r);
+            let p = r.point_at_parameter(2.0);
+            let col: Vec3 = color(&r, &list);
             let ir: u32 = (255.99 * col[0]) as u32;
             let ig: u32 = (255.99 * col[1]) as u32;
             let ib: u32 = (255.99 * col[2]) as u32;

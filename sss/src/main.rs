86115ac4613926b1bc95a9669cc01fa3d3f3c4c4
Bilal Durrani <bilal33@gmail.com>
2020-04-11 23:18:23 -0400

    fix up table generation

--- sss/src/main.rs
+++ sss/src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 use clap::App;
-use std::convert::TryInto;
+// use std::convert::TryInto;
 
 // https://crypto.stackexchange.com/questions/12873/aes-mixcolumn-stage/12880#12880
 
@@ -14,12 +14,12 @@ fn field_int(degree: i32) {}
 
 fn mul(a1: u8, b1: u8) -> u8 {
     // https://crypto.stackexchange.com/questions/12873/aes-mixcolumn-stage/12880#12880
-    let mut a: u8 = 0xb6;
-    let mut b: u8 = 0x53;
+    let mut a: u8 = a1;
+    let mut b: u8 = b1;
     let mut p: u8 = 0;
     let mut carry: u8;
 
-    println!("starting a: {:b}", a);
+    // println!("starting a: {:b}", a);
     // https://en.wikipedia.org/wiki/Multiplication_algorithm#Binary_or_Peasant_multiplication
     while b != 0 {
         if b & 1 == 1 {
@@ -27,14 +27,27 @@ fn mul(a1: u8, b1: u8) -> u8 {
         }
         carry = a & 0x80u8;
         a <<= 1;
-        println!("a: {:b}", a);
+        // println!("a: {:b}", a);
         if carry != 0 {
             a ^= 0x001b;
         }
         b >>= 1;
-        println!("carry: {:b} a: {:b} b: {:b} p: {:b}", carry, a, b, p);
+        // println!("carry: {:b} a: {:b} b: {:b} p: {:b}", carry, a, b, p);
     }
-    println!("result:{:b} 0x{:X} b:{:}", p, p, b);
+    // println!("result:{:b} 0x{:X} b:{:}", p, p, b);
+    return p;
+}
+
+fn print_exp() {
+    let mut x = 0x01;
+    for power in 0..255usize {
+        let res = mul(x, 0x03u8);
+        print!("0x{:X}", res);
+        if (power + 1) % 15 == 0 {
+            println!();
+        }
+        x = res;
+    }
 }
 
 fn main() {
@@ -43,8 +56,16 @@ fn main() {
         .about("Does great things!")
         .author("Kevin K.")
         .get_matches();
-    println!("Hello, world!");
     let input = "This is a secret";
     let opt_security = MAX_DEGREE;
     let log = [0_u8; 256];
+    print_exp();
+}
+
+#[test]
+fn math_works() {
+    let res = mul(0xb6, 0x53);
+    assert_eq!(res, 0x36);
+    let res = mul(0x1a, 0x53);
+    assert_eq!(res, 0x4f);
 }
